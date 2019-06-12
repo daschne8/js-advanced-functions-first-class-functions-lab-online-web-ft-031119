466310643664a3b77a368765c1331f75d96150a4
Daniel Schneider <daschne8@gmail.com>
2019-06-12 23:35:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const selectingDrivers = [
-  function returnFirstTwoDrivers(drivers){
-    return drivers.slice(0,2)
-  },
-  function returnLastTwoDrivers(drivers){
-    return drivers.slice(-2,drivers.length)
-  }
-]
+function returnFirstTwoDrivers(drivers){
+  return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(drivers){
+  return drivers.slice(-2,drivers.length)
+}
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ]
